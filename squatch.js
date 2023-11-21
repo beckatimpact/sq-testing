e06d1764f8c9f4929585eb6b5c4305ad3098596e
beckatimpact <148280979+beckatimpact@users.noreply.github.com>
2023-11-20 16:58:40 -0800

    Testing engagement medium

--- squatch.js
+++ squatch.js
@@ -1,23 +1,6 @@
 !function(a,b){a("squatch","https://fast.ssqt.io/squatch-js@2",b)}(function(a,b,c){var d,e,f;c["_"+a]={},c[a]={},c[a].ready=function(b){c["_" + a].ready =  c["_" + a].ready || [];c["_" + a].ready.push(b);},e=document.createElement("script"),e.async=1,e.src=b,f=document.getElementsByTagName("script")[0],f.parentNode.insertBefore(e,f)},this);
 
-window.squatchTenant = "test_aym93okveuya4";
 
+window.squatchTenant = "test_acbdw3cgv8n5m";
 
-
-squatch.ready(function () {
-
-  squatch.api().squatchReferralCookie().then(function(response) {
-
-    // Retrieve the full list of codes
-    var codesList = Object.values(response.codes);
-    if(codesList.length > 0){
-       document.getElementById('referralCodes').value = codesList.join(", ");
-    }
-
-    console.log(codesList);
-
-    // // Heirarchy -- will use `example-program` as default or `secondary-program` if no cookie is found for the former
-    // document.getElementById('referralCode').value = response.codes['example-program'] || response.codes['secondary-program']; 
-
-    });
-  });
+window.squatchToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoicmVmZXJyZXJVc2VyIiwiYWNjb3VudElkIjoicmVmZXJyZXJVc2VyIn19.WW7NF2oyL71NV4eK4hHOptJwK_LPxA4vWA_56QTV_PI";
